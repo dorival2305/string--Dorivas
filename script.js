aa7b227136aa80c6146b4d95d2584b483a782bd6
dorival2305 <s.dorival@escola.pr.gov.br>
2025-07-03 01:12:46 +0000

    remover palavras indesejadas, otimizando

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
+
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
@@ -44,7 +46,6 @@ function contaFrequencias(palavras) {
 }
 
 function tiraPalavrasRuins(palavras) {
-    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
     const palavrasBoas = [];
     for (let palavra of palavras) {
         if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
@@ -53,7 +54,4 @@ function tiraPalavrasRuins(palavras) {
     }
     return palavrasBoas;
 }
-
-
-
 //funcionamento do botão//
