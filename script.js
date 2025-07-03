0bb9207fc8e408e85e74d86f24ec2968344e0d76
dorival2305 <s.dorival@escola.pr.gov.br>
2025-07-03 00:10:48 +0000

    adicionei o java, para funcionamento do botão

---
+++ script.js
@@ -0,0 +1,10 @@
+cconst botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
+function mostraPalavrasChave() {
+    alert('fui clicado!');
+}
+
+
+//funcionamento do botão//
